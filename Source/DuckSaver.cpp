b7f43ee87b60d3df170b8820e3da090ddcbd8a90
John Scipione <jscipione@gmail.com>
2014-05-22 20:52:31 +0000

    Update copyright header

--- Source/DuckSaver.cpp
+++ Source/DuckSaver.cpp
@@ -1,11 +1,13 @@
 /*
  * Copyrights (c):
- *     2002 - 2008 , Werner Freytag.
- *     2009, Haiku
+ *     2002-2008, Werner Freytag.
+ *     2009-2014 Haiku, Inc. All rights reserved
  * Distributed under the terms of the MIT License.
  *
  * Original Author:
- *              Werner Freytag <freytag@gmx.de>
+ *		Werner Freytag <freytag@gmx.de>
+ * Updates by:
+ *		John Scipione, jscipione@gmail.com
  */
 
 
